1b3bb58d04af157aa2cc8075638647fa68af032a
denzeochieng denzeochieng <aketchd02@gmail.com>
2025-07-29 15:47:36 +0300

    2

---
+++ ifandelse/test2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	age := 69
+		if age >= 65{
+			fmt.Println("Eligible for senior discount")
+		}else{
+			fmt.Println("Not eligible for senior discount")
+		}
+	}
